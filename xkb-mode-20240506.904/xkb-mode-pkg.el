9e06cba34d443fe961b69cc54498339f959ef3d7
Hayato Ito <hayatoito@gmail.com>
2024-08-20 15:05:18 +0900

    Add xkb-mode

---
+++ xkb-mode-20240506.904/xkb-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "xkb-mode" "20240506.904" "Major mode for editing X Keyboard Extension (XKB) files"
+  '((emacs "25.1"))
+  :commit "b1de5233dc12749a97ad6a63d86b921bf1e33d3b" :authors
+  '(("James Dyer" . "captainflasmr@gmail.com"))
+  :maintainers
+  '(("James Dyer" . "captainflasmr@gmail.com"))
+  :maintainer
+  '("James Dyer" . "captainflasmr@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/captainflasmr/xkb-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
