4256277ee759176143da20842933574b86f52f42
Hayato Ito <hayatoito@gmail.com>
2024-10-14 12:14:48 +0900

    Sync @ hayatox1

---
+++ test-simple-20230916.1634/test-simple-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "test-simple" "20230916.1634"
+  "Simple Unit Test Framework for Emacs Lisp."
+  '((cl-lib "0"))
+  :url "https://github.com/rocky/emacs-test-simple"
+  :commit "8b191842318bb05da74052025192d32ebebb033a"
+  :revdesc "8b191842318b"
+  :keywords '("unit-test")
+  :authors '(("Rocky Bernstein" . "rocky@gnu.org"))
+  :maintainers '(("Rocky Bernstein" . "rocky@gnu.org")))
