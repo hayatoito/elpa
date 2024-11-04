8b70cf0833698b781582aed2237a2a9d2011942a
Hayato Ito <hayatoito@gmail.com>
2024-11-05 01:34:45 +0900

    Sync @ hayatox1

---
+++ gptel-20241103.513/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20241103.513"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.4.0")
+    (compat    "29.1.4.1"))
+  :url "https://github.com/karthink/gptel"
+  :commit "b7ce74bcb777243bf21d20158e83efc889de7c8c"
+  :revdesc "b7ce74bcb777"
+  :keywords '("convenience")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
