e03819e986e6bd634319fd30bf90d3cb764872af
Hayato Ito <hayatoito@gmail.com>
2025-03-29 21:35:08 +0900

    Sync @ hayatox0

---
+++ gptel-aibo-20250318.546/gptel-aibo-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel-aibo" "20250318.546"
+  "An AI Writing Assistant."
+  '((emacs "27.1")
+    (gptel "0.9.7"))
+  :url "https://github.com/dolmens/gptel-aibo"
+  :commit "42be3102de37ec988fde513f6fd0768c164d3b99"
+  :revdesc "42be3102de37"
+  :keywords '("emacs" "tools" "editing" "gptel" "ai" "assistant" "code-completion" "productivity")
+  :authors '(("Sun Yi Ming" . "dolmens@gmail.com"))
+  :maintainers '(("Sun Yi Ming" . "dolmens@gmail.com")))
