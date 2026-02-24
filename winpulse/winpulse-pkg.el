517e179ac545b490e5e8ebd47258fcb90f4ffa8f
Hayato Ito <hayatoito@gmail.com>
2026-02-24 10:18:28 +0900

    Sync @ hayatox0

--- winpulse/winpulse-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from winpulse.el  -*- no-byte-compile: t -*-
-(define-package "winpulse" "0.0.1" "No description available." '((emacs "28.1")) :kind vc :commit "53f77d93e8e479721d1187b12b1fcd9756456b81")
