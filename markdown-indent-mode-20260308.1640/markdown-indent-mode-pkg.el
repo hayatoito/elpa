aabc88242eb98657dca33a3fad614e2e0f7408d2
Hayato Ito <hayatoito@gmail.com>
2026-03-10 22:11:24 +0900

    Sync @ hayatox0

---
+++ markdown-indent-mode-20260308.1640/markdown-indent-mode-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "markdown-indent-mode" "20260308.1640"
+  "Dynamic indentation for Markdown."
+  '((emacs "28.1"))
+  :url "https://github.com/whhone/markdown-indent-mode"
+  :commit "44aa4ddb57c0a0787302ae4ed3e054d9af12004d"
+  :revdesc "44aa4ddb57c0"
+  :authors '(("Wai Hon Law" . "whhone@gmail.com"))
+  :maintainers '(("Wai Hon Law" . "whhone@gmail.com")))
